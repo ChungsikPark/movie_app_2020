82119cad427605bf332ac5f8104b2068b7c4a327
ChungsikPark <qkrcndtlr1234@gmail.com>
2020-02-24 21:19:03 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React from 'react';
 
-// props.favourite = {favourite}
-function Food({favourite}) {
-return <h1>I like {favourite}</h1>;
+// props.name = {name}
+function Food({name, picture}) {
+return <div>
+  <h2>I like {name}</h2>
+  <img src={picture} />
+</div>
 }
+
+const foodIlike = [
+  {
+    name:"Chimaek",
+    image:"https://10mag.com/wp-content/uploads/2018/10/2.-Chimaek.jpg"
+  },
+  {
+    name:"Galbitang",
+    image:"https://10mag.com/wp-content/uploads/2018/10/5.-Galbitang-1.jpg"
+  },
+  {
+    name:"Bingsu",
+    image:"https://10mag.com/wp-content/uploads/2018/10/8.-Bingsu.jpg"
+  },
+  {
+    name:"Pajeon",
+    image:"https://10mag.com/wp-content/uploads/2018/10/9.-Meokgolli-w-Pajeon-2.jpg"
+  },
+  {
+    name:"Jjimdak",
+    image:"https://10mag.com/wp-content/uploads/2018/10/4.-Jjimdak-8.jpg"
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food favourite="kimchi" />
-      <Food favourite="ramen" />
-      <Food favourite="samgiopsal" />
-      <Food favourite="chukumi" />
+      {foodIlike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
