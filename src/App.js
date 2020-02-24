09acc4acfa6d7a80fb32235c1bfe61ef51b05fea
ChungsikPark <qkrcndtlr1234@gmail.com>
2020-02-24 23:43:32 +0900

    #2.3 .map Recap

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
 import React from 'react';
 
-// props.name = {name}
-function Food({name, picture}) {
-return <div>
-  <h2>I like {name}</h2>
-  <img src={picture} />
-</div>
-}
-
 const foodIlike = [
   {
+    id:1,
     name:"Chimaek",
     image:"https://10mag.com/wp-content/uploads/2018/10/2.-Chimaek.jpg"
   },
   {
+    id:2,
     name:"Galbitang",
     image:"https://10mag.com/wp-content/uploads/2018/10/5.-Galbitang-1.jpg"
   },
   {
+    id:3,
     name:"Bingsu",
     image:"https://10mag.com/wp-content/uploads/2018/10/8.-Bingsu.jpg"
   },
   {
+    id:4,
     name:"Pajeon",
     image:"https://10mag.com/wp-content/uploads/2018/10/9.-Meokgolli-w-Pajeon-2.jpg"
   },
   {
+    id:5,
     name:"Jjimdak",
     image:"https://10mag.com/wp-content/uploads/2018/10/4.-Jjimdak-8.jpg"
   }
 ]
 
+// props.name = {name}
+function Food({name, picture}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name}/>
+  </div>
+  }
+
 function App() {
   return (
     <div>
       {foodIlike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
