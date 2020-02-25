4efc744672ee2336cfd15e4104d8606045e4e721
ChungsikPark <qkrcndtlr1234@gmail.com>
2020-02-25 23:28:22 +0900

    protection with proptypes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const foodIlike = [
   {
     id:1,
     name:"Chimaek",
-    image:"https://10mag.com/wp-content/uploads/2018/10/2.-Chimaek.jpg"
+    image:
+      "https://10mag.com/wp-content/uploads/2018/10/2.-Chimaek.jpg",
+    // rating: 5.0
   },
   {
     id:2,
     name:"Galbitang",
-    image:"https://10mag.com/wp-content/uploads/2018/10/5.-Galbitang-1.jpg"
+    image:
+      "https://10mag.com/wp-content/uploads/2018/10/5.-Galbitang-1.jpg",
+    rating: 4.6
   },
   {
     id:3,
     name:"Bingsu",
-    image:"https://10mag.com/wp-content/uploads/2018/10/8.-Bingsu.jpg"
+    image:
+      "https://10mag.com/wp-content/uploads/2018/10/8.-Bingsu.jpg",
+    rating: 4.8
   },
   {
     id:4,
     name:"Pajeon",
-    image:"https://10mag.com/wp-content/uploads/2018/10/9.-Meokgolli-w-Pajeon-2.jpg"
+    image:
+      "https://10mag.com/wp-content/uploads/2018/10/9.-Meokgolli-w-Pajeon-2.jpg",
+    rating: 4.5
   },
   {
     id:5,
     name:"Jjimdak",
-    image:"https://10mag.com/wp-content/uploads/2018/10/4.-Jjimdak-8.jpg"
+    image:
+      "https://10mag.com/wp-content/uploads/2018/10/4.-Jjimdak-8.jpg",
+    rating: 4.7
   }
 ]
 
 // props.name = {name}
-function Food({name, picture}) {
+function Food({name, picture, rating}) {
   return <div>
     <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
     <img src={picture} alt={name}/>
   </div>
-  }
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 function App() {
   return (
     <div>
       {foodIlike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
