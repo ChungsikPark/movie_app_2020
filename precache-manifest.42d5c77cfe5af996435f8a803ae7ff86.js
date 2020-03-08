d4465310aba7baa9352a9da23bb3c0de2890d747
ChungsikPark <qkrcndtlr1234@gmail.com>
2020-03-08 18:52:27 +0900

    Updates

---
+++ precache-manifest.42d5c77cfe5af996435f8a803ae7ff86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48fa112042fbe8a85c8a5bfa1bd2b006",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "255e4311fe6759742b05",
+    "url": "/movie_app_2020/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "e3aca23ad6fcf7f07508",
+    "url": "/movie_app_2020/static/js/2.18b08166.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.18b08166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255e4311fe6759742b05",
+    "url": "/movie_app_2020/static/js/main.50d98945.chunk.js"
+  },
+  {
+    "revision": "21664dc3e2d58fbc7515",
+    "url": "/movie_app_2020/static/js/runtime-main.de86b078.js"
+  }
+]);
